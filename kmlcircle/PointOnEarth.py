d106710e648bedfb37ae37563e28500f0de4f660
tigerlihao <tigerlihao@5d6dcabe-de7e-11de-b4bf-3fb2a63344f9>
2010-03-22 13:09:58 +0000

    change structure

--- kmlcircle/PointOnEarth.py
+++ kmlcircle/PointOnEarth.py
@@ -1,20 +1,11 @@
 import math
 
 class PointOnEarth:
-    tolerance=0.000000001
+    tolerance=0.000000001#error less than 0.1m
     R=6371300.0
     def __init__(self,lon=0.0,lat=0.0):
         self.lon=(lon+180.0) % 360.0 -180.0
-        if lat>90.0:
-            self.lat=90.0
-        elif lat<-90.0:
-            self.lat=-90.0
-        else:
-            self.lat=lat
-    def radToDeg(self,radian):
-        return radian*180.0/math.pi
-    def degToRad(self,degree):
-        return math.pi*degree/180.0
+        self.lat=math.fabs((lat+270.0)%360.0-180.0)-90.0
     def disToRad(self,distance):
         return distance/self.R
     def radToDis(self,radian):
@@ -23,58 +14,64 @@ class PointOnEarth:
         return self.radToDis(
             math.acos(
                 math.cos(
-                    self.degToRad(self.lat)
+                    math.radians(self.lat)
                 )*math.cos(
-                    self.degToRad(p.lat)
+                    math.radians(p.lat)
                 )*math.cos(
-                    self.degToRad(self.lon-p.lon)
+                    math.radians(self.lon-p.lon)
                 )+math.sin(
-                    self.degToRad(self.lat)
+                    math.radians(self.lat)
                 )*math.sin(
-                    self.degToRad(p.lat)
+                    math.radians(p.lat)
                 )
             )
         )
-    def getPointBydirection(self,direction,distance):
-        direct=self.degToRad(direction % 360.0)
-        r=self.degToRad(distance)
-        lon=self.degToRad(self.lon)
-        lat=self.degToRad(self.lat)
-        lonDiff = 0.0
+    def getPointBydirection(self,direct,r):
+        direct=direct % 360.0
+        r=math.fabs((r+180.0)%360.0-180.0)
         p= PointOnEarth()
-        s = math.sin(r) * math.sin(r) * math.cos(direct) + math.cos(r)* math.cos(r)
-        sinLatX = (s * math.cos(lat) - math.cos(r) * math.cos(lat + r))/ math.sin(r)
-        if -sinLatX*sinLatX+1.0<self.tolerance*self.tolerance:#Object Point is polar
-            if direct>(math.pi/2) and direct<(math.pi*3/2):
-                p.lat=-math.pi/2
-            else:
-                p.lat=math.pi/2
-            lonDiff = 0.0
+        if 90.0-self.lat<self.tolerance:#Center Point is North Polar
+            p.lon=self.lon+180.0-direct;
+            p.lat=90.0-r
+        elif self.lat+90.0<self.tolerance:#Center Point is South Polar
+            p.lon=90.0+180.0-direct;
+            p.lat=self.lat+r
         else:
-            p.lat=math.asin(sinLatX)
-            if (math.cos(lat)*math.cos(lat)<self.tolerance*self.tolerance):#Point p is polar.
-                lonDiff=math.pi-direct;
-            else:# Point p is not polar.
-                cosLonDiff=(math.cos(r)-sinLatX*math.sin(lat))/(math.cos(p.lat)*math.cos(lat));
-                print "%.18f,%.18f,%.18f" % (cosLonDiff,(-cosLonDiff*cosLonDiff+1.0),(self.tolerance*self.tolerance))
-                if -cosLonDiff*cosLonDiff+1.0<self.tolerance*self.tolerance:#northward or southward
-                    if cosLonDiff>0.0:
-                        lonDiff = 0.0;
-                    else:
-                        lonDiff=math.pi;
-                else:
-                    lonDiff=math.acos(cosLonDiff)
-                    if direct>math.pi:
-                        lonDiff = -lonDiff
-        print "%.12f" % lonDiff
-        p.lon=lon+lonDiff;
-        #while p.lon>math.pi or p.lon<-math.pi:
-        #    if p.lon>0.0:
-        #        p.lon=p.lon-math.pi*2;
-        #    else:
-        #        p.lon=p.lon+math.pi*2;
-        p.lon=(self.radToDeg(p.lon)+180.0) % 360.0 -180.0
-        p.lat=self.radToDeg(p.lat)
+	        direct=math.radians(direct)
+	        r=math.radians(r)
+	        lon=math.radians(self.lon)
+	        lat=math.radians(self.lat)
+	        lonDiff = 0.0
+
+	        s = math.sin(r) * math.sin(r) * math.cos(direct) + math.cos(r)* math.cos(r)
+	        sinLatX = (s * math.cos(lat) - math.cos(r) * math.cos(lat + r))/ math.sin(r)
+	        if -sinLatX*sinLatX+1.0<self.tolerance*self.tolerance:#Object Point is polar
+	            if direct>(math.pi/2) and direct<(math.pi*3/2):
+	                p.lat=-math.pi/2
+	            else:
+	                p.lat=math.pi/2
+	            lonDiff = 0.0
+	        else:
+	            p.lat=math.asin(sinLatX)
+	            if (math.cos(lat)*math.cos(lat)<self.tolerance*self.tolerance):#Point p is polar.
+	                lonDiff=math.pi-direct;
+	            else:# Point p is not polar.
+	                cosLonDiff1=(math.cos(r)-sinLatX*math.sin(lat))/(math.cos(p.lat)*math.cos(lat));
+	                cosLonDiff=-(s * math.sin(lat) - math.cos(r) * math.sin(lat + r))/ (math.sin(r)*math.cos(p.lat));
+	                print "%.18f,%.18f,%.18f,%.18f" % (cosLonDiff1,cosLonDiff,(-cosLonDiff*cosLonDiff+1.0),(self.tolerance*self.tolerance))
+	                if -cosLonDiff*cosLonDiff+1.0<self.tolerance*self.tolerance:#northward or southward
+	                    if cosLonDiff>0.0:
+	                        lonDiff = 0.0;
+	                    else:
+	                        lonDiff=math.pi;
+	                else:
+	                    lonDiff=math.acos(cosLonDiff)
+	                    if direct>math.pi:
+	                        lonDiff = -lonDiff
+	        print "%.12f" % lonDiff
+	        p.lon=lon+lonDiff;
+	        p.lon=(math.degrees(p.lon)+180.0) % 360.0 -180.0
+	        p.lat=math.degrees(p.lat)
         return p
 
 if __name__=="__main__":
