1b9629b413f3952bb200b841816448266fb7f1df
tigerlihao <tigerlihao@5d6dcabe-de7e-11de-b4bf-3fb2a63344f9>
2010-03-24 06:46:04 +0000

    add GetArcHandler.

--- trunk/kmlcircle/main.py
+++ trunk/kmlcircle/main.py
@@ -23,21 +23,23 @@ from PointOnEarth import PointOnEarth
 
 class GetCircleHandler(webapp.RequestHandler):
   def post(self):
-    if self.request.get('lon')=="":
-      lon=0.0
-    else:
-      lon=float(self.request.get('lon'))
-    if self.request.get('lat')=="":
-      lat=0.0
-    else:
-      lat=float(self.request.get('lat'))
-    if self.request.get('r')=="":
-      r=90.0
-    else:
-      r=float(self.request.get('r'))
-    self.response.headers['Content-Type'] = "application/vnd.google-earth.kml+xml"
-    self.response.out.write("""<Folder>
-    <name>KML Circle Generator Output</name>
+    try:
+      if self.request.get('lon')=="":
+        lon=0.0
+      else:
+        lon=float(self.request.get('lon'))
+      if self.request.get('lat')=="":
+        lat=0.0
+      else:
+        lat=float(self.request.get('lat'))
+      if self.request.get('r')=="":
+        r=90.0
+      else:
+        r=float(self.request.get('r'))
+      #self.response.headers['Content-Type'] = "application/vnd.google-earth.kml+xml"
+      self.response.headers['Content-Type'] = "text/xml"
+      self.response.out.write("""<Folder>
+    <name>Your Circle</name>
     <visibility>1</visibility>
     <Placemark>
         <name>circle</name>
@@ -48,17 +50,76 @@ class GetCircleHandler(webapp.RequestHandler):
         </Style>
         <LineString>
             <coordinates>""")
-    p=PointOnEarth(lon,lat)
-    for i in range(0,361,5):
-      p2=p.getPointBydirection(i,r)
-      self.response.out.write('%.8f,%.8f ' % (p2.lon,p2.lat))
-    self.response.out.write("""</coordinates>
+      p=PointOnEarth(lon,lat)
+      for i in range(0,361,5):
+        p2=p.getPointBydirection(i,r)
+        self.response.out.write('%.8f,%.8f ' % (p2.lon,p2.lat))
+      self.response.out.write("""</coordinates>
         </LineString>
     </Placemark>
 </Folder>""")
+    except (TypeError, ValueError):
+      self.response.out.write("<html><body><h1>Invalid inputs!</h1></body></html>")
+  def get(self):
+    self.post()
+
+class GetArcHandler(webapp.RequestHandler):
+  def post(self):
+    try:
+      if self.request.get('lon')=="":
+        lon=0.0
+      else:
+        lon=float(self.request.get('lon'))
+      if self.request.get('lat')=="":
+        lat=0.0
+      else:
+        lat=float(self.request.get('lat'))
+      if self.request.get('r')=="":
+        r=90.0
+      else:
+        r=float(self.request.get('r'))
+      if self.request.get('start')=="":
+        start=0.0
+      else:
+        start=float(self.request.get('start'))
+      if self.request.get('end')=="":
+        end=90.0
+      else:
+        end=float(self.request.get('end'))
+      self.response.headers['Content-Type'] = "text/xml"
+      self.response.out.write("""<Folder>
+    <name>Your Arc</name>
+    <visibility>1</visibility>
+    <Placemark>
+        <name>circle</name>
+        <visibility>1</visibility>
+        <Style>
+            <geomColor>ff0000ff</geomColor>
+            <geomScale>1</geomScale>
+        </Style>
+        <LineString>
+            <coordinates>""")
+      p=PointOnEarth(lon,lat)
+      sect=3.0
+      l=(end-start)%360.0
+      n=math.ceil(l/sect)
+      sect=l/n
+      #self.response.out.write('%.8f,%.8f ' % (p.lon,p.lat))
+      for i in range(n+1):
+        p2=p.getPointBydirection((start+sect*i),r)
+        self.response.out.write('%.8f,%.8f ' % (p2.lon,p2.lat))
+      #self.response.out.write('%.8f,%.8f ' % (p.lon,p.lat))
+      self.response.out.write("""</coordinates>
+        </LineString>
+    </Placemark>
+</Folder>""")
+    except (TypeError, ValueError):
+      self.response.out.write("<html><body><h1>Invalid inputs!</h1></body></html>")
+  def get(self):
+    self.post()
 
 def main():
-  application = webapp.WSGIApplication([('/circle', GetCircleHandler)],debug=True)
+  application = webapp.WSGIApplication([('/circle', GetCircleHandler),('/arc',GetArcHandler)],debug=True)
   util.run_wsgi_app(application)
 
 
